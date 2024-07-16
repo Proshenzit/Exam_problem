acb86c2a6b7e6d6acf01765b27c7effc2ddfe67b
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-07-16 02:35:02 -0700

    Add files via upload

---
+++ programiz7.py
@@ -0,0 +1,6 @@
+score = int (input("Enter  your  score : "))
+if score>=50:
+    print("you passed in the examination ")
+    print("congratulations ")
+if score <50:
+    print("sorry, you failed in the exam")
